e9f32845c9c017403af46c6c6a345f2f0a37441e
Andreas Smolenko <a.smolenko@fz-juelich.de>
2023-10-19 15:24:21 +0200

    Kollaborationsbeispiel vorbereiten von person1

---
+++ test.sql
@@ -0,0 +1,9 @@
+<verkehrInformation>
+  <strasse id="7123">
+    <name>Hauptstrasse</name>
+    <parkplaetze>154</parkplaetze>
+    <verkehrStatus>moderat</verkehrStatus>
+    <geschwindigkeitsbegrenzung>50</geschwindigkeitsbegrenzung>
+    <strasseZustand>einwandfrei</strasseZustand>
+  </strasse>
+</verkehrInformation>
